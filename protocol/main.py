1048c73ea3248d6123d9eb0a3ad3461a19fada3c
kimsehwanestat <sktpghks138@gmail.com>
2020-06-23 19:24:40 +0900

    change some line

--- protocol/main.py
+++ protocol/main.py
@@ -6,26 +6,18 @@ from data_manager import DataFramer
 from data_manager import Command
 from data_manager import DataGather
 
-
-
-count = 0
 data_gather = DataGather('127.0.0.1', 9999)
 
 def thread_run():   
-    global count
-    count += 1
 
-    print(count)
     print("function executed ! {}".format(datetime.datetime.now()))
-
     oper = data_gather.get_operands()
     data_gather.factory(oper).do() #factory design
-    
     t = threading.Timer(5, thread_run)
     t.start()
-    if count > 5:
-        t.cancel()
-        count = 0
+
+    #TODO: 5분마다 TDAT 로직 run, 
+    # 새로운 명령어가 들어올 경우 해당 명령어 처리.
 
 if __name__ == "__main__":
     thread_run()
