299592f176b3076473d07c38b30218ef87d5121c
kimsehwanestat <sktpghks138@gmail.com>
2020-06-25 00:54:40 +0900

    add sls & codes for restful api

---
+++ aws_study/todo_api/todos/create.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import os
+import time
+import uuid
+
+import boto3 # for aws service connect
+
+dynamodb = boto3.resource('dynamodb')
+
+def create(event, context):
+    data = json.loads(event['body'])
+    if 'text' not in data:
+        logging.error("Vaildation Failed")
+        raise Exception("could not create todo item")
+
+    timestamp = str(time.time())
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TAZBLE'])
+
+    item = {
+        'id' : str(uuid.uuid1()),
+        'text' : data['text'],
+        'checked' : False,
+        'createdAt' : timestamp,
+        'updatedAt' : timestamp
+    }
+
+    table.put_item(Item=item)
+
+    response = {
+        "statusCode" : 200,
+        "body" : json.dumps(item)
+    }
+
+    return response
