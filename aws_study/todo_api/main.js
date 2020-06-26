f52b4685310bd9a19ecff04a533b4ea2ce600719
kimsehwanestat <sktpghks138@gmail.com>
2020-06-27 02:04:05 +0900

    DynamoDB 로부터 데이터 받아서 출력

--- aws_study/todo_api/main.js
+++ aws_study/todo_api/main.js
@@ -6,4 +6,15 @@ var settings = {
   
   $.ajax(settings).done(function (response) {
     console.log(response);
+    //const json = response;
+    //const obj = JSON.parse(json);
+    //console.log(obj[0].text)
+    console.log(response[0].text);
+    console.log(response.length);
+    const idx = response.length;
+    for (i = 0; i <= idx; i++){
+        document.write("<h1>");
+        document.write(response[i].text);
+        document.write("</h1>");
+    };
   });
