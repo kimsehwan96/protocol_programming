cdf1a9cd2665d1fd60e2af65435baf7bab63dc13
kimsehwanestat <sktpghks138@gmail.com>
2020-06-27 01:47:32 +0900

    add header for cors

--- aws_study/todo_api/main.js
+++ aws_study/todo_api/main.js
@@ -1,9 +1,7 @@
 var settings = {
     "url": "https://9eeme9fjb9.execute-api.ap-northeast-2.amazonaws.com/dev/todos",
     "method": "GET",
-    "timeout": 0,
-    "crossOrigin" : true,
-    "dataType" : "json",
+    "timeout": 0
   };
   
   $.ajax(settings).done(function (response) {
