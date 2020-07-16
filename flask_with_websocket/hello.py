a491d6fa449d1a05a0bbe5a9cf8ccf58ee2e4663
kimsehwanestat <sktpghks138@gmail.com>
2020-07-17 02:17:30 +0900

    change some

--- flask_with_websocket/hello.py
+++ flask_with_websocket/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 #from flask.ext.socketio import SOcketIO, emit
 
 import random
@@ -19,10 +19,22 @@ def render_index(var=TEST_VALUE):
     var = TEST_VALUE
     return render_template('index.html',var=var)
 
+#api ?
+@app.route('/data')
+def data_api():
+    return jsonify({
+        "InputValue_1" : random.randint(1, 100),
+        "InputValue_2" : random.randint(1, 100),
+        "InputValue_3" : random.randint(1, 100),
+        "RTDValue_1" : random.randint(1, 100)
+    })
+
 def making_number():
     global TEST_VALUE
     TEST_VALUE = random.randint(1,100)
     threading.Timer(1,making_number).start()
+
+#TODO: socketIO 적용
 
 if __name__ == '__main__':
     making_number()
